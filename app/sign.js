0f01fde109cd52fb56ca260e0ad400e69d980774
Ken Eldridge <kenneth.eldridge@gmail.com>
2018-08-30 22:07:28 -0700

    Update sign.js

--- app/sign.js
+++ app/sign.js
@@ -1,4 +1,4 @@
-const script = 'sudo /home/pi/Muni/rpi-rgb-led-matrix/examples-api-use/text-example --led-rows=16 --led-cols=32 --led-chain=3 -S 0 -b 50 -f ./app/muni.bdf -C 177,74,5 --led-parallel=1 --led-pwm-lsb-nanoseconds=50 --led-slowdown-gpio=4';
+const script = 'sudo /home/pi/Muni/rpi-rgb-led-matrix/examples-api-use/text-example --led-rows=16 --led-cols=32 --led-chain=3 -S 0 -b 100 -f ./app/muni.bdf -C 177,74,5 --led-parallel=1 --led-pwm-lsb-nanoseconds=50 --led-slowdown-gpio=4';
 
 let child;
 const exec = require('child_process').exec;
