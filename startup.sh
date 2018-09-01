17dfe6c7a451c893c7b490a74cf8a87925ebddbf
Ken Eldridge <kenneth.eldridge@gmail.com>
2018-08-31 22:59:20 -0700

    changing startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+while ! ping -c 1 -W 1 4.2.2.1; do
+    echo "Waiting for 1.2.3.4 - network interface might be down..."
+    sleep 1
+done
+
 cd /home/pi/Muni/MuniSign/
 git fetch --all
 git reset --hard origin/master
