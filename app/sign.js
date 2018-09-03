58d495e0af3aabac8eaf1fff1cb3acf1fd8b06d1
Ken Eldridge <kenneth.eldridge@gmail.com>
2018-09-02 21:11:32 -0700

    user permisisons console

--- app/sign.js
+++ app/sign.js
@@ -1,7 +1,7 @@
 const script = 'sudo /home/pi/Muni/rpi-rgb-led-matrix/examples-api-use/text-example --led-rows=16 --led-cols=32 --led-chain=3 -S 0 -b 100 -f ./app/muni.bdf -C 177,74,5 --led-parallel=1 --led-pwm-lsb-nanoseconds=50 --led-slowdown-gpio=4';
 const { exec } = require('child_process');
 
-let child = exec(script, (error, stdout, stderr) => {
+let child = exec(script, {uid: 1000} (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
